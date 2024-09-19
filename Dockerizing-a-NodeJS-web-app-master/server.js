367ae7b05ab40655c2151df2ac532090451d114f
HiteshTP <155635465+HiteshTP@users.noreply.github.com>
2024-09-19 13:30:43 +0530

    Update server.js

--- Dockerizing-a-NodeJS-web-app-master/server.js
+++ Dockerizing-a-NodeJS-web-app-master/server.js
@@ -1,16 +1,9 @@
 'use strict';
-
-const express = require('express');
-
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
-
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello world\n');
-});
-
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+   var express = require('express');
+   var app = express();
+   app.get('/', function (req, res) {
+     res.send('Hello World!');
+   });
+   app.listen(8000, function () {
+     console.log('Example app listening on port 8000!');
+   });
